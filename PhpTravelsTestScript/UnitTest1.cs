80c6908d95f2f5c07ddf56862abe31c5e294b9b1
CelesQYZheng <celes.qy.zheng@gmail.com>
2020-04-27 13:52:39 +0800

    first commit

---
+++ PhpTravelsTestScript/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhpTravelsTestScript
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
